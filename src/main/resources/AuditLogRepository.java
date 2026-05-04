a31792d7ac5a8b6c064f3556a5551bdb322a1f76
deepa2043 <nandish3369@gmail.com>
2026-05-04 19:56:13 +0530

    remove wrongly placed repository file

--- src/main/resources/AuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
-}
